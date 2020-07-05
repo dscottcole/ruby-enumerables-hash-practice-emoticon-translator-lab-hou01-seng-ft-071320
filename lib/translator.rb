5d82ee8eb6585b37b2ca860b7db2bd449c087324
Daniel Cole <danielscole1@gmail.com>
2020-07-05 20:47:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,20 +19,32 @@ end
 def get_japanese_emoticon(file_path, western_emoticon)
   # code goes here
   load_library(file_path)
+<<<<<<< HEAD
   emoticon_hash = load_library(file_path)
   japanese_emoticon = "Sorry, that emoticon was not found"
+=======
+  japanese_emoticon = nil
+  emoticon_hash = load_library(file_path)
+>>>>>>> 61d505c7071b1ca75a8e045718dd8f409d1433aa
   
   emoticon_match = emoticon_hash.select{|key, hash| hash[:english] == western_emoticon}
     if emoticon_match != {}
       emoticon_match.each do |k,v|
       japanese_emoticon = emoticon_match[k][:japanese]
+<<<<<<< HEAD
     end
+=======
+    else   
+      japanese_emoticon = "Sorry, #{western_emoticon} isn't a known emoticon"
+    end  
+>>>>>>> 61d505c7071b1ca75a8e045718dd8f409d1433aa
   end
 japanese_emoticon  
 end
 
 
 
+<<<<<<< HEAD
 def get_english_meaning(file_path, japanese_emoticon)
   # code goes here
  emoticon_hash = load_library(file_path)
@@ -45,4 +57,11 @@ def get_english_meaning(file_path, japanese_emoticon)
     end
   end
 english_meaning  
+=======
+def get_english_meaning
+  # code goes here
+  load_library(file_path)
+  
+  
+>>>>>>> 61d505c7071b1ca75a8e045718dd8f409d1433aa
 end
